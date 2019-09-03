0c6dea56ba8fec2ca4382c84bc7a2e683ce854b9
Partast1 <43130360+Partast1@users.noreply.github.com>
2019-09-03 17:29:39 +0200

    Quiz

---
+++ SolSystem/SolarSystemQuizz/QuizRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SolarSystemQuizz
+{
+    public class QuizRepository
+    {
+        private static string connectionString = "User Id=root;Host=localhost;Database=solarsystem";
+
+        public Quiz
+    }
+}
